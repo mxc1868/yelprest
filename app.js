7d7ad24fe0b06bc350528ecafd136b6a9923416f
mxc1868 <mxc1868@gmail.com>
2017-08-30 16:19:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var CommentSchema = require("./models/comments");
 var UserSchema = require("./models/users");
 var seed = require("./seed");
 //connect MongoDB
-mongoose.connect("mongodb://mxc1868:mxc1868@cluster0-shard-00-00-kbwok.mongodb.net:27017,cluster0-shard-00-01-kbwok.mongodb.net:27017,cluster0-shard-00-02-kbwok.mongodb.net:27017/yelprest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+mongoose.connect("");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
